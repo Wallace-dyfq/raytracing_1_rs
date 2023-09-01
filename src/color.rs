6a6f8e4bcc536ce60c31a1674c65adf1645435b9
wallace <wallace.dyfq@gmail.com>
2023-08-31 21:20:53 -0600

    color and write color

---
+++ src/color.rs
@@ -0,0 +1,11 @@
+use crate::vec3::Vec3;
+use crate::Result;
+use std::io::Write;
+pub type Color = Vec3;
+pub fn write_color<W>(writer: &mut W, c: &Color) -> Result<()> 
+where W: Write {
+    write!(writer, "{} {} {}\n", (c.x().clone() * 255.999) as u32,
+                (c.y().clone() * 255.999) as u32,
+                (c.z() * 255.999) as u32)?;
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+mod vec3;
+mod color;
+use std::env;
+
+use vec3::Vec3;
+use color::write_color;
+use color::Color;
+pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    //let width: u32 = args[1].parse().unwrap_or(256_u32);
+    let width: u32 = args
+        .get(1)
+        .unwrap_or(&"256".to_string())
+        .parse()
+        .unwrap_or(256_u32);
+    let height: u32 = args
+        .get(2)
+        .unwrap_or(&"256".to_string())
+        .parse()
+        .unwrap_or(256_u32);
+    println!("P3\n{} {}\n255", width, height);
+    for i in 0..height {
+        for j in 0..width {
+            let r = i as f64 / (width - 1) as f64;
+            let g = j as f64 / (width - 1) as f64;
+            let b = 0.0;
+            let _ = write_color(&mut std::io::stdout(), &Color::new(r, g, b));
+        }
+    }
+}
