e871ffabbdeb04f402d61e269aa4a62445721599
wallace <wallace.dyfq@gmail.com>
2023-09-01 18:50:53 -0600

    simplify ray sphere intersection code

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,13 @@ pub type Result<T> = std::result::Result<T, Error>;
 fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> f64 {
     let oc = &ray.orig - center;
     let a = ray.dir.dot(&ray.dir);
-    let b = 2.0 * oc.dot(&ray.dir);
+    let half_b = oc.dot(&ray.dir);
     let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+    let discriminant = half_b * half_b - a * c;
     if discriminant < 0.0 {
         return -1.0;
     } else {
-        let result = (-b - discriminant.sqrt()) / (2.0 * a);
+        let result = (-half_b - discriminant.sqrt()) / a;
         return result;
     }
 }
