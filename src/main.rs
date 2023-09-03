a47958cbfd0ff9b1a8396a44bfe24a5d9ef12054
wallace <wallace.dyfq@gmail.com>
2023-09-03 17:02:47 -0600

    final scene

--- src/main.rs
+++ src/main.rs
@@ -26,51 +26,81 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut world = Hittables::default();
     //meterial
-    let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Rc::new(Dielectric::new(1.5));
-    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
-    // world
-    let world: Hittables = Hittables {
-        objects: vec![
-            Box::new(Sphere::new(
-                Point3::new(0.0, -100.5, -1.0),
-                100.0,
-                material_ground.clone(),
-            )),
-            Box::new(Sphere::new(
-                Point3::new(0.0, 0.0, -1.0),
-                0.5,
-                material_center.clone(),
-            )),
-            Box::new(Sphere::new(
-                Point3::new(-1.0, 0.0, -1.0),
-                0.5,
-                material_left.clone(),
-            )),
-            Box::new(Sphere::new(
-                Point3::new(-1.0, 0.0, -1.0),
-                -0.4,
-                material_left.clone(),
-            )),
-            Box::new(Sphere::new(
-                Point3::new(1.0, 0.0, -1.0),
-                0.5,
-                material_right.clone(),
-            )),
-        ],
-    };
+    let material_ground = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        material_ground.clone(),
+    )));
+
+    let p = Point3::new(4.0, 0.2, 0.0);
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = utils::random_f64();
+            let center = Point3::new(
+                a as f64 + utils::random_f64(),
+                0.2,
+                b as f64 + utils::random_f64(),
+            );
+
+            if (&center - &p).length() > 0.9 {
+                match choose_mat {
+                    x if x < 0.8 => {
+                        // difuse
+                        let sphere_material = Rc::new(Lambertian::new(
+                            &Color::random(0.0, 1.0) * &Color::random(0.0, 1.0),
+                        ));
+                        world.add(Box::new(Sphere::new(center, 0.2, sphere_material.clone())));
+                    }
+                    x if x < 0.95 => {
+                        // matel
+                        let albedo = Color::random(0.5, 1.0);
+                        let fuzz = utils::random_f64_range(0.0, 0.5);
+                        let sphere_material = Rc::new(Metal::new(albedo, fuzz));
+                        world.add(Box::new(Sphere::new(center, 0.2, sphere_material.clone())));
+                    }
+                    _ => {
+                        // glass
+                        let sphere_material = Rc::new(Dielectric::new(1.5));
+                        world.add(Box::new(Sphere::new(center, 0.2, sphere_material.clone())));
+                    }
+                };
+            }
+        }
+    }
+
+    let material_1 = Rc::new(Dielectric::new(1.5));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        material_1.clone(),
+    )));
+    let material_2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    world.add(Box::new(Sphere::new(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material_2.clone(),
+    )));
+    let material_3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    world.add(Box::new(Sphere::new(
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        material_3.clone(),
+    )));
 
     // camera
     let mut camera = Camera::new(
         16.0 / 9.0,
-        400,  /* iamge width*/
-        100,  /* sample per pixel */
+        1200, /* image width*/
+        500,  /* sample per pixel */
         50,   /* max depth */
         20.0, /* vfov */
     );
-    camera.look_from = Point3::new(-2.0, 2.0, 1.0);
-    camera.look_at = Point3::new(0.0, 0.0, -1.0);
+    camera.look_from = Point3::new(13.0, 2.0, 3.0);
+    camera.look_at = Point3::new(0.0, 0.0, 0.0);
+    camera.defocus_angle = 0.6;
+    camera.focus_dist = 10.0;
     camera.render(&world);
 }
